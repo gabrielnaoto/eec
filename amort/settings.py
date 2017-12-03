7575188f68db3ddd5900c6787ef1126d30a096ef
Gabriel Naoto @ Dark-Eagle <gabrielnaoto@yahoo.com.br>
2017-12-03 18:16:01 -0200

    settings of templates

--- amort/settings.py
+++ amort/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'amort.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['./templates'],
+        'DIRS': ['./amort/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
