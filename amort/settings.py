b96466af689ac1af42c4c8b721cdd756b5b491c7
Gabriel Naoto @ Dark-Eagle <gabrielnaoto@yahoo.com.br>
2017-12-03 18:06:34 -0200

    oops, adding dburl import

--- amort/settings.py
+++ amort/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from decouple import config
 
+from dj_database_url import parse as dburl  
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
