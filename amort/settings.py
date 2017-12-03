4a35fbbbf6efe0716bcfe1837a52637f230edab6
Gabriel Naoto @ Dark-Eagle <gabrielnaoto@yahoo.com.br>
2017-12-03 18:08:42 -0200

    n precisa de dois staticfilesdirs

--- amort/settings.py
+++ amort/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from decouple import config
 
-from dj_database_url import parse as dburl  
+from dj_database_url import parse as dburl
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -132,7 +132,3 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT,'amort', 'static'),
-)
